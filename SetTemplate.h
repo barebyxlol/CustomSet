40b7ba5bfc6c3caac68515e274c49e2ab8847d2b
barebyxlol <100487614+barebyxlol@users.noreply.github.com>
2022-03-20 19:08:58 +0300

    Update SetTemplate.h

--- SetTemplate.h
+++ SetTemplate.h
@@ -1,10 +1,11 @@
 #include <cstddef>
 #include <algorithm>
 
+// Template set class, based on AVL-tree. https://en.wikipedia.org/wiki/AVL_tree
 
 template<class T>
 class Set {
-private:
+  private:
     struct node {
         T key;
         size_t height;
@@ -32,12 +33,16 @@ private:
 
     };
 
+    // Returns height of a tree vertex.
+
     size_t GetHeight(node *v) const {
         if (v != nullptr) {
             return v->height;
         }
         return 0;
     }
+
+    // Returns balance factor of a tree vertex.
 
     int GetBalance(node *v) const {
         if (v != nullptr) {
@@ -46,11 +51,15 @@ private:
         return 0;
     }
 
+    // Fixes height field of a vertex, if it is not correct.
+
     void FixHeight(node *v) {
         if (v != nullptr) {
             v->height = std::max(GetHeight(v->left), GetHeight(v->right)) + 1;
         }
     }
+
+    // Next two methods implement right and left rotation of a vertex to rebalance the tree. Complexity O(1).
 
     node *RightRotation(node *v) {
         node *q = v->left;
@@ -80,6 +89,8 @@ private:
         return q;
     }
 
+    // Fixes the tree if the current vertex needs to be rebalanced. Complexity O(1).
+
     node *FixBalance(node *v) {
         if (v == nullptr) {
             return nullptr;
@@ -102,6 +113,8 @@ private:
         return v;
     }
 
+    // Inserts a new element into the tree. Complexity O(log n).
+
     node *Insert(node *v, const T &k, node *par) {
         if (v == nullptr) {
             return new node(k, par);
@@ -115,6 +128,8 @@ private:
         return v;
     }
 
+    // Finds minimal element in the subtree of a current vertex. Complexity O(log n).
+
     node *FindMin(node *v) const {
         if (v->left == nullptr) {
             return v;
@@ -123,6 +138,8 @@ private:
         }
     }
 
+    // Finds maximal element in the subtree of a current vertex. Complexity O(log n).
+
     node *FindEnd(node *v) const {
         if (v->right == nullptr) {
             return v;
@@ -130,6 +147,8 @@ private:
             return FindEnd(v->right);
         }
     }
+
+    // Erases minimal element in the subtree of the current vertex. Complexity O(log n).
 
     node *EraseMin(node *v) {
         if (v->left == nullptr) {
@@ -142,6 +161,9 @@ private:
         v = FixBalance(v);
         return v;
     }
+
+    // Erases vertex in the tree with the given key value or
+    // does nothing if such vertex does not exist. Complexity O(log n).
 
     node *Erase(node *v, const T &k) {
         if (v == nullptr) {
@@ -179,6 +201,8 @@ private:
         return v;
     }
 
+    // Finds a vertex with the given key value or returns nullptr if such vertex does not exist. Complexity O(log n).
+
     node *Find(node *v, const T &k) const {
         if (v == nullptr) {
             return nullptr;
@@ -199,6 +223,8 @@ private:
 
     }
 
+    // Finds a vertex with the minimal value more or equal to the given key value. Complexity O(log n).
+
     node *LowerBound(node *v, node *par, const T &k) const {
         if (v == nullptr) {
             return par;
@@ -217,15 +243,18 @@ private:
         return v;
     }
 
+    // Deallocates the memory of the whole tree.
+
     void DestroySet(node *v) {
         if (v == nullptr) {
-            delete v;
             return;
         }
         DestroySet(v->left);
         DestroySet(v->right);
         delete v;
     }
+
+    // Creates a deep copy of a given tree.
 
     node *CopyNode(node *v, node *par) {
         if (v == nullptr) {
@@ -242,143 +271,154 @@ private:
         return n;
     }
 
-public:
+  public:
+
+    // Iterator class for the set, using pointer to const node to operate.
+    // Supports the similar methods as the STL set iterator.
 
     class iterator {
-    public:
+      public:
         iterator() = default;
 
-        explicit iterator(node *v) : it(v) {}
+        explicit iterator(node *v) : it_(v) {}
 
-        iterator(const iterator &iter) : it(iter.it) {}
+        iterator(const iterator &iter) : it_(iter.it_) {}
 
         iterator &operator=(const iterator &iter) {
             if (this == &iter) {
                 return *this;
             }
-            it = iter.it;
+            it_ = iter.it_;
             return *this;
         }
 
         bool operator==(const iterator &iter) const {
-            return it == iter.it;
+            return it_ == iter.it_;
         }
 
         bool operator!=(const iterator &iter) const {
-            return it != iter.it;
+            return it_ != iter.it_;
         }
 
+        // Next four methods implement increments and decrements of an iterator.
+        // Incrementing past-the-last element iterator or decrementing first element iterator causes undefined behaviour.
+        // The transition to the next element may take up to O(log n) operations, but passage through the entire set
+        // takes O(n) operations.
+
+
         iterator &operator++() {
-            if (it->is_end) {
+            if (it_->is_end) {
                 return *this;
             }
-            if (it->right != nullptr) {
-                it = it->right;
-                while (it->left != nullptr) {
-                    it = it->left;
+            if (it_->right != nullptr) {
+                it_ = it_->right;
+                while (it_->left != nullptr) {
+                    it_ = it_->left;
                 }
                 return *this;
             }
-            T t = it->key;
-            while (!it->is_end && !(t < it->key) && it->parent != nullptr) {
-                it = it->parent;
+            T t = it_->key;
+            while (!it_->is_end && !(t < it_->key) && it_->parent != nullptr) {
+                it_ = it_->parent;
             }
             return *this;
         }
 
         iterator &operator--() {
-            if (it->is_end) {
-                if (it->left != nullptr) {
-                    it = it->left;
+            if (it_->is_end) {
+                if (it_->left != nullptr) {
+                    it_ = it_->left;
                 } else {
-                    it = it->parent;
+                    it_ = it_->parent;
                 }
                 return *this;
             }
-            if (it->left != nullptr) {
-                it = it->left;
-                while (it->right != nullptr) {
-                    it = it->right;
+            if (it_->left != nullptr) {
+                it_ = it_->left;
+                while (it_->right != nullptr) {
+                    it_ = it_->right;
                 }
                 return *this;
             }
-            T t = it->key;
-            if (it->parent == nullptr) {
+            T t = it_->key;
+            if (it_->parent == nullptr) {
                 return *this;
             }
-            if (it->parent->is_end) {
-                if (it->parent->parent == nullptr) {
+            if (it_->parent->is_end) {
+                if (it_->parent->parent == nullptr) {
                     return *this;
                 }
-                it = it->parent->parent;
+                it_ = it_->parent->parent;
             }
-            while (!(it->key < t) && it->parent != nullptr) {
-                it = it->parent;
+            while (!(it_->key < t) && it_->parent != nullptr) {
+                it_ = it_->parent;
             }
             return *this;
         }
 
         iterator &operator++(int) {
-            if (it->is_end) {
+            if (it_->is_end) {
                 return *this;
             }
-            if (it->right != nullptr) {
-                it = it->right;
-                while (it->left != nullptr) {
-                    it = it->left;
+            if (it_->right != nullptr) {
+                it_ = it_->right;
+                while (it_->left != nullptr) {
+                    it_ = it_->left;
                 }
                 return *this;
             }
-            T t = it->key;
-            while (!(t < it->key) && it->parent != nullptr) {
-                it = it->parent;
+            T t = it_->key;
+            while (!(t < it_->key) && it_->parent != nullptr) {
+                it_ = it_->parent;
             }
             return *this;
         }
 
         iterator &operator--(int) {
-            if (it->is_end) {
-                if (it->left != nullptr) {
-                    it = it->left;
+            if (it_->is_end) {
+                if (it_->left != nullptr) {
+                    it_ = it_->left;
                 } else {
-                    it = it->parent;
+                    it_ = it_->parent;
                 }
                 return *this;
             }
-            if (it->left != nullptr) {
-                it = it->left;
-                while (it->right != nullptr) {
-                    it = it->right;
+            if (it_->left != nullptr) {
+                it_ = it_->left;
+                while (it_->right != nullptr) {
+                    it_ = it_->right;
                 }
                 return *this;
             }
-            T t = it->key;
-            if (it->parent == nullptr) {
+            T t = it_->key;
+            if (it_->parent == nullptr) {
                 return *this;
             }
-            if (it->parent->is_end) {
-                if (it->parent->parent == nullptr) {
+            if (it_->parent->is_end) {
+                if (it_->parent->parent == nullptr) {
                     return *this;
                 }
-                it = it->parent->parent;
+                it_ = it_->parent->parent;
             }
-            while (!(it->key < t) && it->parent != nullptr) {
-                it = it->parent;
+            while (!(it_->key < t) && it_->parent != nullptr) {
+                it_ = it_->parent;
             }
             return *this;
         }
 
         T operator*() const {
-            return it->key;
+            return it_->key;
         }
 
         const T *operator->() const {
-            return &(it->key);
+            return &(it_->key);
         }
 
-    private:
-        const node *it;
+      private:
+        const node *it_;
     };
+
+    // Default set constructor.
 
     Set() {
         root_ = new node(nullptr);
@@ -386,12 +426,16 @@ public:
         end_ = root_;
     }
 
+    // Inserts element with the given value to the set.
+
     void insert(const T &k) {
         if (Find(root_, k) == nullptr) {
             ++size_;
             root_ = Insert(root_, k, nullptr);
         }
     }
+
+    // Constructor from the given sequence of elements specified by the begin and end iterators.
 
     template<typename Iterator>
     Set(Iterator beginit, Iterator endit) {
@@ -401,6 +445,8 @@ public:
         std::for_each(beginit, endit, [this](const T &k) { (*this).insert(k); });
     }
 
+    // Initialiser list constructor.
+
     Set(std::initializer_list<T> lst) {
         size_ = 0;
         root_ = new node(nullptr);
@@ -408,11 +454,15 @@ public:
         std::for_each(lst.begin(), lst.end(), [this](const T &k) { (*this).insert(k); });
     }
 
+    // Copy constructor.
+
     Set(const Set &st) {
         size_ = st.size_;
         root_ = CopyNode(st.root_, nullptr);
         end_ = FindEnd(root_);
     }
+
+    // Copy assignment operator.
 
     Set &operator=(const Set &st) {
         if (this == &st) {
@@ -429,13 +479,20 @@ public:
         DestroySet(root_);
     }
 
-    size_t size() const {
+    // Returns the number of elements in the set.
+
+    [[nodiscard]] size_t size() const {
         return size_;
     }
 
-    bool empty() const {
+    // Returns true if the set is empty.
+
+    [[nodiscard]] bool empty() const {
         return size_ == 0;
     }
+
+    // Returns an iterator to the element with the given key or past-the-end iterator if no such element is found.
+    // Complexity O(log n).
 
     iterator find(T k) const {
         node *v = Find(root_, k);
@@ -445,6 +502,8 @@ public:
         return iterator(v);
     }
 
+    // Erases an element with the given key or does nothing if no such element is found. Complexity O(log n).
+
     void erase(T k) {
         if (Find(root_, k) != nullptr) {
             --size_;
@@ -452,13 +511,19 @@ public:
         }
     }
 
+    // Returns iterator to the first element.
+
     iterator begin() const {
         return iterator(FindMin(root_));
     }
 
+    // Return past-the-end iterator.
+
     iterator end() const {
         return iterator(end_);
     }
+
+    // Returns iterator to the first element with the value more or equal to the given key. Complexity O(log n).
 
     iterator lower_bound(const T &k) const {
         auto iter = iterator(LowerBound(root_, nullptr, k));
@@ -469,7 +534,7 @@ public:
         return iter;
     }
 
-private:
+  private:
     node *root_;
     size_t size_;
     node *end_;
